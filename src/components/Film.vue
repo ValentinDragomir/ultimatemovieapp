6b1387318c0d4300640f3a43c069803852444cc4
ValentinDragomir <valentindragomir@live.com>
2020-10-05 20:37:15 +0300

    OMDb API test

---
+++ src/components/Film.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="film">
+        <div v-bind="film" v-if="film.Error!='Movie not found!'">
+            <img :src="film.Poster">
+            <h2>{{ film.Title }}</h2>
+            <h4>Year: {{ film.Year }}</h4>
+            <h4>Genre: {{ film.Genre }}</h4>
+            <h4>IMDb Score: {{ film.imdbRating }} / 10</h4>
+            <h4>IMDb Votes: {{ film.imdbVotes }}</h4>
+            <h4>Metascore: {{ film.Metascore }}</h4>
+            <h4>Runtime: {{ film.Runtime }}</h4>
+            <h4>Director: {{ film.Director }}</h4>
+            <h4>Actors: {{ film.Actors }}</h4>
+            <h4>Production: {{ film.Production }}</h4>
+            <h4>Awards: {{ film.Awards }}</h4>
+            <h4>Synopsis: {{ film.Plot }}</h4>
+        </div>
+        <div v-bind="film" v-if="film.Error=='Movie not found'">
+            <h4>{{ film.Error }}</h4>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['film']
+}
+</script>
+
+<style scoped>
+.film img {
+    float: left;
+    margin-right: 40px;
+}
+.film h2 {
+    margin-top: 23px;
+}
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="search">
+        <input type="text" name="search" placeholder="Enter movie title" v-model="query" @keypress.enter="handleSearch">
+        <button v-on:click="handleSearch">Search</button>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            query: ''
+        }
+    },
+    methods: {
+        handleSearch() {
+            if (this.query=="") {
+                alert("You have to enter a movie title")
+            } else {
+                this.$emit('requestedSearch', this.query)
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.search input {
+    padding: 10px;
+    width: 400px;
+    border-radius: 10px;
+}
+.search button {
+    padding: 10px;
+    margin-left: 5px;
+    width: 100px;
+    border-radius: 10px;
+    background-color: lightsteelblue;
+    color: darkblue
+}
+</style>

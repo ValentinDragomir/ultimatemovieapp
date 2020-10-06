2de9feb771e810c22409e375c94df905af314472
ValentinDragomir <valentindragomir@live.com>
2020-10-07 00:58:28 +0300

    Vuetify theme toggler

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,56 @@
 <template>
-  <div id="app">
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Logo"
+          class="shrink mr-2"
+          contain
+          src=""
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src=""
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-tooltip v-if="!$vuetify.theme.dark" bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn v-on="on" color="info" small fab @click="darkMode">
+            <v-icon class="mr-1">mdi-moon-waxing-crescent</v-icon>
+          </v-btn>
+        </template>
+        <span>Dark Mode On</span>
+      </v-tooltip>
+
+      <v-tooltip v-else bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn v-on="on" color="info" small fab @click="darkMode">
+            <v-icon color="yellow">mdi-white-balance-sunny</v-icon>
+          </v-btn>
+        </template>
+        <span>Dark Mode Off</span>
+      </v-tooltip>
+    </v-app-bar>
+
+    <v-main>
     <Search v-on:requestedSearch="handleSearch"></Search>
     <Film v-bind:film="film"></Film>
-  </div>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
@@ -29,6 +77,9 @@ export default {
           this.film = res
           console.log(this.film.error)
         })
+    },
+    darkMode() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
     }
   }
 }
@@ -40,7 +91,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #1d55bd;
   margin-top: 60px;
 }
 </style>

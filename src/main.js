892db291ab144890fa3bc7bdfc760b69b2330e23
ValentinDragomir <valentindragomir@live.com>
2023-09-07 16:55:22 +0300

    Automatic system theme detection

--- src/main.js
+++ src/main.js
@@ -6,5 +6,17 @@ Vue.config.productionTip = false
 
 new Vue({
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+
+    beforeMount () {
+    // check if browser supports dark mode.
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme)').media !== 'not all') {
+
+			// if user prefers dark mode switch to dark mode
+      if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        this.$vuetify.theme.isDark = true;
+      }
+    }
+  }
+
 }).$mount('#app')
